a08976136d0f488872da0a13050aff924274f0ce
MartialTarizzo <43091489+MartialTarizzo@users.noreply.github.com>
2023-12-11 18:05:23 +0100

    réglages cosmétiques

--- mkgrid.py
+++ mkgrid.py
@@ -4,9 +4,9 @@ from gen_calisson import randomEnigma2
 from html_calisson import make_url
 import pyscript
 
-
 @pyscript.when("click","#mkgrid")
 def mkGrid(event):
+
     elem_n = pyscript.document.querySelector("#taille")
     n = int(elem_n.value)
     elem_easy = pyscript.document.querySelector("#facilite")
@@ -17,7 +17,7 @@ def mkGrid(event):
     elem_info.innerText = "Calcul de la grille en cours ..."
 
     deb = time.monotonic()
-    enigme = randomEnigma2(n, [], trace = False, easy = eas)
+    enigme = randomEnigma2(n, [], trace = True, easy = eas)
     elem_info.innerText = f"Grille calculée en {time.monotonic()-deb:.2f} s"
     
     url = make_url(enigme, n)
